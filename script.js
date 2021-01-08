264fbe99bfa1f77489aa6cefcd8e0075f012bea0
Egor Sizov <mrjarokvk@gmail.com>
2021-01-08 15:01:03 +0300

    123

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var
   ctx = canv.getContext("2d");
 canv.width = 450;
 canv.height = 800;
-ctx.fillStyle = "black";
 ctx.fillRect(0, 150, 450, 10);
 ctx.fillRect(0, 300, 450, 10);
 ctx.fillRect(150, 0, 10, 450);
@@ -13,6 +12,32 @@ var a = [[0,0,0],
          [0,0,0],
          [0,0,0]];
 var img = new Image();
+function clean(){
+  ctx.clearRect(0, 0, 450, 450);
+  ctx.fillRect(0, 150, 450, 10);
+  ctx.fillRect(0, 300, 450, 10);
+  ctx.fillRect(150, 0, 10, 450);
+  ctx.fillRect(300, 0, 10, 450);
+  a = [[0,0,0],
+       [0,0,0],
+       [0,0,0]];
+}
+function winner0(){
+  clean();
+  z = 1;
+}
+function winnerX(){
+  img.onload = function(){
+    clean();
+  }
+  z = 0;
+}
+function pat(){
+  img.onload = function(){
+    clean();
+  }
+  z = 0;
+}
 function draw(x, y, z){
   if (z % 2 == 0){
     ctx.beginPath();
@@ -28,24 +53,31 @@ function draw(x, y, z){
   }
   let s3 = 0;
       s4 = 0;
+      num = 0;
   for (let i = 0; i < 3; i++){
     let s1 = 0;
         s2 = 0;
     for (let j = 0; j < 3; j++){
       s1 += a[i][j];
       s2 += a[j][i];
+      if (a[i][j] > 0)
+        num += 1;
       if (i == j)
         s3 += a[i][j];
       if (i + j == 2)
         s4 += a[i][j];
       if (s1 == 12 || s2 == 12 || s3 == 12 || s4 == 12){
         console.log("0");
+        winner0();
         break;
       }
       if (s1 == 9 || s2 == 9 || s3 == 9 || s4 == 9){
         console.log("x");
+        winnerX();
         break;
       }
+      else if (num == 9)
+        pat();
     }
   }
 }

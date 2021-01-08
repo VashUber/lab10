8aa0f6fd7767559c658a3995344c9c405819aca4
Egor Sizov <mrjarokvk@gmail.com>
2021-01-08 14:08:52 +0300

    пока так

--- script.js
+++ script.js
@@ -9,42 +9,96 @@ ctx.fillRect(0, 300, 450, 10);
 ctx.fillRect(150, 0, 10, 450);
 ctx.fillRect(300, 0, 10, 450);
 let z = 0;
-var a = [0, 0, 0,
-         0, 0, 0,
-         0, 0, 0]
+var a = [[0,0,0],
+         [0,0,0],
+         [0,0,0]];
 var img = new Image();
 function draw(x, y, z){
-    if (z % 2 == 0){
-      ctx.beginPath();
-      ctx.lineWidth = 6;
-      ctx.arc(x, y, 30, 0, Math.PI * 2, false);
-      ctx.stroke();
+  if (z % 2 == 0){
+    ctx.beginPath();
+    ctx.lineWidth = 6;
+    ctx.arc(x, y, 30, 0, Math.PI * 2, false);
+    ctx.stroke();
+  }
+  else{
+    img.onload = function(){
+      ctx.drawImage(img, x-35, y-35, 70, 70);
     }
-    else{
-      img.onload = function(){
-        ctx.drawImage(img, x-35, y-35, 70, 70);
+    img.src = "x.png";
+  }
+  let s3 = 0;
+      s4 = 0;
+  for (let i = 0; i < 3; i++){
+    let s1 = 0;
+        s2 = 0;
+    for (let j = 0; j < 3; j++){
+      s1 += a[i][j];
+      s2 += a[j][i];
+      if (i == j)
+        s3 += a[i][j];
+      if (i + j == 2)
+        s4 += a[i][j];
+      if (s1 == 12 || s2 == 12 || s3 == 12 || s4 == 12){
+        console.log("0");
+        break;
       }
-       img.src = "x.png";
+      if (s1 == 9 || s2 == 9 || s3 == 9 || s4 == 9){
+        console.log("x");
+        break;
+      }
     }
+  }
 }
 function check(x, y, z){
   if(x <= 150){
     if (y <= 150)
       if (z % 2 == 0)
-        a[0] = 4;
+        a[0][0] = 4;
       else
-        a[0] = 3;
+        a[0][0] = 3;
     else if (y <= 300)
         if (z % 2 == 0)
-          a[3] = 4;
+          a[1][0] = 4;
         else
-          a[3] = 3;
+          a[1][0] = 3;
       else
         if (z % 2 == 0)
-          a[6] = 4;
+          a[2][0] = 4;
         else
-          a[6] = 3;
+          a[2][0] = 3;
   }
+  else if(x <= 300)
+    if (y <= 150)
+      if (z % 2 == 0)
+        a[0][1] = 4;
+      else
+        a[0][1] = 3;
+    else if (y <= 300)
+        if (z % 2 == 0)
+          a[1][1] = 4;
+        else
+          a[1][1] = 3;
+      else
+        if (z % 2 == 0)
+          a[2][1] = 4;
+        else
+          a[2][1] = 3;
+    else
+      if (y <= 150)
+        if (z % 2 == 0)
+          a[0][2] = 4;
+        else
+          a[0][2] = 3;
+      else if (y <= 300)
+          if (z % 2 == 0)
+            a[1][2] = 4;
+          else
+            a[1][2] = 3;
+        else
+          if (z % 2 == 0)
+            a[2][2] = 4;
+          else
+            a[2][2] = 3;
   draw(x, y, z);
   console.log(a)
 }
